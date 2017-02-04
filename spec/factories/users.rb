7643c1a11e0de87cd87165d794ddd1aeac7fbcf1
NovitskyiDA <allfayer@gmail.com>
2017-02-05 01:09:47 +0200

    Add user registration

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:username) { |n| FFaker::InternetSE.user_name_variant_long[0..8].sub('.', '_') + n.to_s }
+    email { "#{username}@#{FFaker::Internet.domain_name}" }
+    password '12341234'
+    auth_token { SecureRandom.base64 }
+  end
+end
